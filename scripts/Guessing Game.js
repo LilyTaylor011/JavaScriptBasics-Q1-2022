0bf5334d1c7e9447afffe2bde6c7680935c72706
Lily Taylor <taylorlm1@s.dcsdk12.org>
2022-08-22 16:06:30 -0600

    Added Variables and started the Guessing Game

---
+++ scripts/Guessing Game.js
@@ -0,0 +1,19 @@
+//@ts-check
+
+const button = document.getElementById ("guessing-game");
+
+console.log(button);
+
+button?.addEventListener("click", function(){
+    // console.log("Clicked");
+    let answer = 7;
+
+let guess = Number(prompt("Pick a number between 1 and 10"));
+
+console.log(guess);
+
+if(answer == guess) {
+    alert ("You won!");
+}
+console.log (guess);
+});
---
+++ scripts/variables.js
@@ -0,0 +1,44 @@
+//@ts-check
+// Anything I type Here is a Comment - Single Line
+
+/* 
+Anything between the opening and closeing are 
+comments and will be ignored by the compiler 
+*/
+
+// declare variable age and assign it the value 15
+let age = 15;
+let gpa= 3.09678412598650;
+
+let someValue = gpa / age;
+
+const myName = "Lily Mae";
+//this line will cause an error if uncommented
+//myName = 42;
+
+//bolean values are only true or false
+let isThisClassAwesome = true;
+console.log (myName);
+
+// define an object called car
+
+let car = {
+    year: 2021,
+    make:"Tesla", 
+    model: "electric", 
+    isClean: false 
+};
+
+car.year = 2021;
+car.color = "gray";
+console.log(car); 
+
+// declare an array of fruits 
+let fruits = [
+    "apple",
+    "pear",
+    "grapes",
+    "banana"
+];
+console.log(fruits [2]);
+
